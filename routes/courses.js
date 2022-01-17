fb72903c3135c43f642fc960bcc50b10e053dda2
Sagyam <sagyamthapa32@gmail.com>
2022-01-17 11:18:21 +0545

    6:10

--- routes/courses.js
+++ routes/courses.js
@@ -4,12 +4,14 @@ const {
 	getCourses,
 	getCourse,
 	createCourse,
+	updateCourse,
+	deleteCourse,
 } = require("../controllers/courses");
 const { route } = require("./bootcamps");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses).post(createCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
